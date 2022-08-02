967a1c396b491e5c0c8e55d33e970f656bd6076d
Sven Trieflinger <sven.trieflinger@de.bosch.com>
2022-08-02 16:12:33 +0200

    Reenable castor client tests after moving to dedicated package
    Signed-off-by: Sven Trieflinger <sven.trieflinger@de.bosch.com>

---
+++ klyshko-operator/castor/suite_test.go
@@ -0,0 +1,27 @@
+/*
+Copyright (c) 2022 - for information on the respective copyright owner
+see the NOTICE file and/or the repository https://github.com/carbynestack/klyshko.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package castor
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"testing"
+
+	. "github.com/onsi/gomega"
+	//+kubebuilder:scaffold:imports
+)
+
+var _ = BeforeSuite(func() {
+	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+})
+
+func TestCastorClient(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Klyshko Castor Client Suite")
+}
