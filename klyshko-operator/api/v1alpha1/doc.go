1a32ccded997e76ec1372fc7cc42645fe04f7104
Sven Trieflinger <sven.trieflinger@de.bosch.com>
2022-09-09 15:31:03 +0200

    Add package documentation
    Signed-off-by: Sven Trieflinger <sven.trieflinger@de.bosch.com>

---
+++ klyshko-operator/api/v1alpha1/doc.go
@@ -0,0 +1,10 @@
+/*
+Copyright (c) 2022 - for information on the respective copyright owner
+see the NOTICE file and/or the repository https://github.com/carbynestack/klyshko.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package v1alpha1 contains the code defining the v1alpha1 version of the
+// Klyshko API.
+package v1alpha1
---
+++ klyshko-operator/controllers/doc.go
@@ -0,0 +1,10 @@
+/*
+Copyright (c) 2022 - for information on the respective copyright owner
+see the NOTICE file and/or the repository https://github.com/carbynestack/klyshko.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package controllers contains the K8s controllers and utilities that
+// implement the Klyshko operator core logic.
+package controllers
